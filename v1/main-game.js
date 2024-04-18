ba23a85a0c27c58d7cb037ebbebbf4b24d7aad2f
waldemar99 <waldemar.blex@googlemail.com>
2024-04-18 15:12:23 +0200

    bugs deleted (alert from the begining and hidde numbers after pressing "new game"

---
+++ v1/main-game.js
@@ -0,0 +1,98 @@
+function game() {
+  let aZZ = Number($("aZZ").value); // anzahlZufallsZahlen
+  {
+    // Get a reference to the parent element
+    const parentElement = $("div-span-wrap");
+    // Loop through and remove all child nodes
+    while (parentElement.firstChild) {
+      parentElement.removeChild(parentElement.firstChild);
+    }
+  }
+
+  for (let i = 0; i < aZZ; i++) {
+    const newSpan = document.createElement("span");
+    newSpan.setAttribute("class", "zz");
+
+    $("div-span-wrap").appendChild(newSpan);
+  }
+
+  {
+    for (let i = 0; i < aZZ; i++) {
+      document.getElementsByClassName("zz")[i].hidden = false;
+    }
+  }
+
+  const arryOfSecretNumbers = [];
+
+  for (let i = 0; i < aZZ; i++) {
+    const xi = Math.floor(Math.random() * 100);
+    console.log(xi);
+    document.getElementsByClassName("zz")[i].innerHTML = `${xi}`;
+    arryOfSecretNumbers.push(xi);
+  }
+  console.log(arryOfSecretNumbers);
+
+  // Zufallszahlen nach 3 sec ausblenden
+
+  let i = document.getElementsByClassName("zz").length - 1;
+
+  const interval = setInterval(() => {
+    console.log(document.getElementsByClassName("zz").length);
+    console.log(i);
+
+    if (i >= 0) {
+      document.getElementsByClassName("zz")[i].hidden = true;
+      i--;
+      console.log(i);
+    } else {
+      clearInterval(interval);
+    }
+  }, 2000);
+
+  // Zufallszahl mit Eingabewert vergleichen
+  //
+
+  document.addEventListener("dblclick", (e) => {
+    $("div-wrap").style.position = "relative";
+    $("div-wrap").style.left = `${e.pageX}px`;
+    console.log(e.pageX);
+    $("div-wrap").style.top = `${e.pageY}px`;
+  });
+}
+
+$("btn-send").addEventListener("click", vergleich);
+
+document.addEventListener("", function (e) {
+  if (e.keyCode === 13) {
+    vergleich;
+  }
+});
+function vergleich() {
+  console.log($("input").value);
+  if ($("input").value === "") {
+    alert("Bitte Schätzung eingeben!");
+    return;
+  }
+  let meinTip = Number($("input").value);
+  $("input").value = "";
+
+  if (arryOfSecretNumbers.includes(meinTip)) {
+    $("ergebnis").innerHTML = "Richtig";
+    arryOfSecretNumbers.splice(arryOfSecretNumbers.indexOf(meinTip), 1);
+    console.log(arryOfSecretNumbers);
+
+    if (arryOfSecretNumbers.length === 0) {
+      alert("Alle Zahlen gewusst");
+      return;
+    }
+    return;
+  }
+
+  document.getElementById(
+    "ergebnis"
+  ).innerText = `Falsch! Richtig wäre ${arryOfSecretNumbers.toString()}`;
+}
+// Querry Function
+function $(str) {
+  return document.getElementById(str);
+}
