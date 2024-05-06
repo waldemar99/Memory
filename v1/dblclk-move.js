fea79d38d15909bdb71657ee5151cc22ce49c8d0
waldemar99 <waldemar.blex@googlemail.com>
2024-05-06 16:24:02 +0200

    change Projektbeschreibung .txt & dblclk-move.js

--- v1/dblclk-move.js
+++ v1/dblclk-move.js
@@ -1,7 +1,7 @@
 document.addEventListener("dblclick", (e) => {
   console.log(e.target);
   if (!($("body-complete") === e.target || $("div-wrap") === e.target)) {
-    $("body-complete").style.position = "relative";
+    $("body-complete").style.position = "absolute";
     $("body-complete").style.left = `${e.pageX}px`;
     console.log(e.pageX);
     $("body-complete").style.top = `${e.pageY}px`;
